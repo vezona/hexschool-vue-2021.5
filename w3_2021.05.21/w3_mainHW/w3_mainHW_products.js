cabce3fddfb18f7efc58a770ba0f9e02495d1f27
vezonaChin <vezona5tv@gmail.com>
2021-07-09 09:43:07 +0800

    修理編輯紐直接新增項目

--- w3_2021.05.21/w3_mainHW/w3_mainHW_products.js
+++ w3_2021.05.21/w3_mainHW/w3_mainHW_products.js
@@ -87,7 +87,7 @@ const product = {
             let http = 'post';
 
             // 修改商品 [API]: /api/:api_path/admin/product/:id ；PUT
-            if (this.isNew) {
+            if (this.isNewTitle) {
                 editProductURL = `${this.apiURL}/${this.apiPath}/admin/product/${this.tempProduct.id}`;
                 http = "put"
             }
